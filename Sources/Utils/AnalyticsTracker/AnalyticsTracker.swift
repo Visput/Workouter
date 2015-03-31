ed8e98d7cff5240b30229802dfc6a31fac9f6f36
Vladimir Popko <vvv.popko@gmail.com>
2015-03-30 21:55:43 -0500

    Added Flurry SDK

---
+++ Sources/Utils/AnalyticsTracker/AnalyticsTracker.swift
@@ -0,0 +1,14 @@
+//
+//  AnalyticsTracker.swift
+//  SportTime
+//
+//  Created by Vladimir Popko on 3/30/15.
+//  Copyright (c) 2015 visput. All rights reserved.
+//
+
+import Foundation
+
+class AnalyticsTracker {
+    let flurryKey = "VM4SSBGSTNJCCWQRZS5D"
+    let flurryKeyQA = "RZYXH9G79VK4J3WP8N92"
+}
