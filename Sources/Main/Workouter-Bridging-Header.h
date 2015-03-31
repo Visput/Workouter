0cf727fd396ba3cffab609d62997a27ba8239482
Vladimir Popko <vvv.popko@gmail.com>
2015-03-31 00:44:44 -0500

    Added Facebook SDK

--- Sources/Main/Workouter-Bridging-Header.h
+++ Sources/Main/Workouter-Bridging-Header.h
@@ -10,5 +10,8 @@
 #define Workouter_Workouter_Bridging_Header_h_h
 
 #import "Flurry.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKShareKit/FBSDKShareKit.h>
 
 #endif
